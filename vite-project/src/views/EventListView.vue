d22ef5be3707aa8174552d0378671dc7fd77a490
nureman <nureman_s@cmu.ac.th>
2023-07-23 21:31:55 +0700

    not sure

--- vite-project/src/views/EventListView.vue
+++ vite-project/src/views/EventListView.vue
@@ -2,6 +2,14 @@
 import EventCard from '../components/EventCard.vue'
 import EventOrganizer from '../components/EventOrganizer.vue'
 import { events } from '@/event_type'
+import type { EventItem } from './type'
+import { ref } from 'vue'
+import EventServices from '@/services/EventService'
+import type { Ref } from 'vue'
+const event: Ref<Array<EventItem>> = ref([])
+EventServices.get().then((response) => {
+  event.value = response.data
+})
 </script>
 
 <template>
